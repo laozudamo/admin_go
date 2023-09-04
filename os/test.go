b5479fecbc050094369e223187f87273b560f8ad
laozudamo <laozudamo@gmail.com>
2023-09-04 11:04:43 +0800

    feat: os 文件

--- os/test.go
+++ os/test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,7 +12,11 @@ func main() {
 
 	//Writefile()
 
-	OpenFile()
+	//OpenFile()
+
+	//readDir()
+
+	handleFile()
 }
 
 func Readfile() {
@@ -67,4 +73,38 @@ func OpenFile() {
 	//file.Seek(0, io.SeekStart)
 	//file.WriteString("Hello world")
 	//r := bufio.NewReader(file)
+}
+
+func readDir() {
+	d, _ := os.ReadDir("os")
+	//fmt.Println(d, err)
+	for _, v := range d {
+		fmt.Println(v.Name(), v.IsDir())
+	}
+}
+
+func handleFile() {
+	f, err := os.OpenFile("os/test.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	writer := bufio.NewWriter(f)
+	reader := bufio.NewReader(f)
+	n := 0
+	for {
+		n++
+		//str, _, err := reader.ReadLine()
+
+		//str, err := reader.ReadString('\n')
+
+		str, err := reader.ReadString('\n')
+		writer.WriteString(strconv.Itoa(n) + "" + str)
+		if err != nil {
+			break
+		}
+	}
+	f.Seek(0, 0)
+	writer.Flush()
 }
